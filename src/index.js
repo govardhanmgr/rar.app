fd67b67db235d6f2e6c644b51066f9c683989d1d
govardhanmgr <morsu.gr000@gmail.com>
2022-05-27 15:40:46 +0530

    update

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import launchPage from './components/launchPage.vue'
+import LdbPage from './components/LdbPage.vue'
+import PadPage from './components/PadPage.vue'
+import ReportPage from './components/ReportPage.vue'
+
+const routes = [
+    {
+        path: '/launchPage',
+        name: 'launchPage',
+        component: launchPage
+  },
+
+  {
+    path: '/LdbPage',
+    name: 'LdbPage',
+    component: LdbPage
+  },
+
+  {
+    path: '/PadPage',
+    name: 'PadPage',
+    component: PadPage
+  },
+
+  {
+    path: '/ReportPage',
+    name: 'ReportPage',
+    component: ReportPage
+  },
+  
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
