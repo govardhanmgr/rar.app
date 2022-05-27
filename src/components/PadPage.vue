e98baf3286056fa7a530ef31da495dddadd42ad6
govardhanmgr <morsu.gr000@gmail.com>
2022-05-27 16:24:36 +0530

    table changes

---
+++ src/components/PadPage.vue
@@ -0,0 +1,18 @@
+<template>
+    Portal Access for Data Management  Page reached
+
+
+    <router-link to="/LdbPage">  <button class="left">Load Data in Batch </button>
+            
+            </router-link>
+            <router-view/>
+
+</template>
+
+
+
+<script>
+    export default {
+        name: 'PadPage',
+    }
+</script>
